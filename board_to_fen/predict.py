9383c6346e4df2801ea4bb1469d6fbda44f5a833
santiagoahl <santiago.ahumada.1@gmail.com>
2025-06-18 22:50:47 -0500

    Update KerasNeuralNetwork and predict modules to make these compatible with python>=3.11

--- board_to_fen/predict.py
+++ board_to_fen/predict.py
@@ -25,24 +25,29 @@ model.load_model_from_weights(path=PATH_TO_MODEL_WEIGHTS)
 # model = tf.keras.models.load_model("board_to_fen/saved_models/november_model_weights.h5")
 
 def get_fen_from_image_path(image_path, end_of_row='/', black_view=False) -> str:
+    """
+    Predict FEN position from path to a chess image.
+
+    Parameters
+    ----------
+    image : type
+        Description
+    end_of_row : str
+        Indicate how to process end of chess row
+    black_biew : bool
+        Pass as False iff the board is set form the player with white chess pieces
+    
+    Returns:
+        str: Predicted chess position in FEN notation
+    """
     image = Image.open(image_path)
     decoder = Decoder_FEN()
-    net = KerasNeuralNetwork()
-    # f = pkg_resources.open_text(saved_models, 'november_model')  # Deprecated
-    #net.load_model(f.name)
+    # net = KerasNeuralNetwork()
     tiler = Tiler()
     tiles = tiler.get_tiles(img=image)
 
-    # Load model as a keras layer
-    #keras_layer = tf.keras.layers.TFSMLayer(
-    #    filepath=PATH_TO_MODEL,
-    #    call_endpoint='serving_default',
-    #    call_training_endpoint=None,
-    #    trainable=True,
-    #    name="chess board to fen notation model",
-    #    dtype=None
-    #)
-
+    # f = pkg_resources.open_text(saved_models, 'november_model')  # Deprecated
+    #net.load_model(f.name)
     #predictions = net.predict(tiles=tiles) # Deprecated
 
     # convert tiles object to tf tensor before compute predictions
@@ -53,21 +58,27 @@ def get_fen_from_image_path(image_path, end_of_row='/', black_view=False) -> str
     return fen
 
 def get_fen_from_image(image, end_of_row='/', black_view=False) -> str:
+    """
+    Predict FEN position from Image-like object, which represents a chess board.
+
+    Parameters
+    ----------
+    image : PIL.Image
+        image Object 
+    end_of_row : str
+        Indicate how to process end of chess row
+    black_biew : bool
+        Pass as False iff the board is set form the player with white chess pieces
+    
+    Returns:
+        str: Predicted chess position in FEN notation
+    """
     decoder = Decoder_FEN()
     tiler = Tiler()
     tiles = tiler.get_tiles(img=image)
 
     tiles_array = np.array([np.array(tile) for tile in tiles], dtype=np.float32)
     tiles_tensor = tf.convert_to_tensor(tiles_array, dtype=tf.float32)
-
-    #keras_layer = tf.keras.layers.TFSMLayer(
-    #    filepath=PATH_TO_MODEL,  
-    #    call_endpoint='serving_default',
-    #    call_training_endpoint=None,
-    #    trainable=False,
-    #    name="chess board to fen notation model",
-    #    dtype=None
-    #)
 
     # Predict
     predictions = model.predict(tiles_tensor)
@@ -79,7 +90,6 @@ def get_fen_from_image(image, end_of_row='/', black_view=False) -> str:
         black_view=black_view
     )
     return fen
-
 
 if __name__ == "__main__":
     img_path = "./board_to_fen/test_image.jpeg"
