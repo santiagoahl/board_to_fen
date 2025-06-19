4bec56a6bd40341879055e002c045396f35fc7e5
santiagoahl <santiago.ahumada.1@gmail.com>
2025-06-19 09:45:28 -0500

    Solve cuda warnings

--- board_to_fen/KerasNeuralNetwork.py
+++ board_to_fen/KerasNeuralNetwork.py
@@ -1,16 +1,20 @@
 import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # Ignore warnings (E.g. CUDA drivers, tf, etc.)
 from contextlib import redirect_stderr
 with redirect_stderr(open(os.devnull, "w")):
     from keras import models, layers
 import numpy as np
 
+INPUT_SHAPE = (50, 50, 3)
 
 class KerasNeuralNetwork:
     def __init__(self) -> None:
         self.CATEGORIES = ["bishop_black", "bishop_white","empty","king_black","king_white","knight_black", "knight_white","pawn_black", "pawn_white", "queen_black","queen_white", "rook_black","rook_white"]
         self.predictions = []
         self.model = models.Sequential()
-        self.model.add(layers.Conv2D(50, (3,3), activation='relu', input_shape=(50,50,3)))
+        self.model.add(layers.Input(shape=INPUT_SHAPE))
+        #self.model.add(layers.Conv2D(50, (3,3), activation='relu', input_shape=(50,50,3)))  # DEPRECATED
+        self.model.add(layers.Conv2D(50, (3,3), activation='relu'))
         self.model.add(layers.MaxPooling2D((2,2)))
         self.model.add(layers.Conv2D(100,(3,3), activation='relu'))
         self.model.add(layers.MaxPooling2D((2,2)))
@@ -40,7 +44,8 @@ class KerasNeuralNetwork:
     def predict(self, tiles) -> list:
         for image in tiles:
             image = np.array(image)
-            image = np.reshape(image, ((1, 50, 50, 3)))
+            image = np.reshape(image, (INPUT_SHAPE))
+            image = np.expand_dims(image, axis=0)
             prediction = self.model.predict(image, verbose=0)
             index = np.argmax(prediction)
             self.predictions.append(self.CATEGORIES[index])
